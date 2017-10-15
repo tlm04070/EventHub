a12ece6263cfe96d821564c140ee499b0fc7a488
Trevor Meadows <tlm04070@gmail.com>
2017-10-15 13:26:00 -0400

    added firebase and started to work on displaying cards

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,93 @@
 $(document).ready(function () {
+
+    var config = {
+        apiKey: "AIzaSyAqvI_ClOC4GsGp582OWw7d7PzFi_pT8sQ",
+        authDomain: "recent-name.firebaseapp.com",
+        databaseURL: "https://recent-name.firebaseio.com",
+        projectId: "recent-name",
+        storageBucket: "recent-name.appspot.com",
+        messagingSenderId: "126583712962"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
     var nameArray = [];
     var dateArray = [];
     var localeArray = [];
-    var authKey = "c6Pfj5qgjtkvDTOAzxWf1bDTtr80wGVA";
-    var searchKeyword = "rock"
-    var searchCity = "charlotte"
-    var queryURLBase = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchKeyword + "&city=" + searchCity + "&raduis=20&unit=miles&apikey=" +
-        authKey;
+    var cityArray = [];
+    var stateArray = [];
 
-    $.ajax({
-        url: queryURLBase,
-        method: "GET"
-    }).done(function (response) {
-        console.log(response);
-        var results = response;
-        // console.log(results._embedded.events[0]);
-        // console.log(results._embedded.events);
-
-        for (var i = 0; i < results._embedded.events.length; i++) {
-            var eventList = results._embedded.events[i];
-            var eventName = eventList.name;
-            var eventDate = eventList.dates.start.localDate;
-
-            nameArray.push(results._embedded.events[i]);
-            dateArray.push(eventDate);
-            localeArray.push();
-            console.log(eventName);
-            console.log(eventDate);
+    $("#infoSubmit").on("click", function () {
 
 
-        };
-        console.log(nameArray[0].name);
-        console.log(nameArray[1].name);
-        console.log(nameArray[2].name);
-        console.log(nameArray[3].name);
-        console.log(dateArray[0]);
-        console.log(dateArray[1]);
-        console.log(dateArray[2]);
-        console.log(dateArray[3]);
+        var searchKeyword = $("#exampleInputEmail1").val().trim();
+        var searchCity = $("#exampleInputPassword1").val().trim();
+        var authKey = "c6Pfj5qgjtkvDTOAzxWf1bDTtr80wGVA";
+        var queryURLBase = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchKeyword + "&city=" + searchCity + "&raduis=20&unit=miles&apikey=" +
+            authKey;
 
 
+        $.ajax({
+            url: queryURLBase,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+            var results = response;
+
+            for (var i = 0; i < results._embedded.events.length; i++) {
+                var eventList = results._embedded.events[i];
+                var eventName = eventList.name;
+                var eventDate = eventList.dates.start.localDate;
+                var eventLocale = eventList._embedded.venues[0].name;
+                var eventCity = eventList._embedded.venues[0].city.name;
+                var eventState = eventList._embedded.venues[0].state.name;
+
+                nameArray.push(results._embedded.events[i].name);
+                dateArray.push(eventDate);
+                localeArray.push(eventLocale);
+                cityArray.push(eventCity);
+                stateArray.push(eventState);
+
+            };
+
+            database.ref().child("Search").set({
+                name: nameArray,
+                date: dateArray,
+                venue: localeArray,
+                city: cityArray,
+                state: stateArray,
+
+            });
+
+        });
+        database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+            console.log(childSnapshot.val().name);
+
+            // Store everything into a variable.
+            var searchName = childSnapshot.val().name;
+            var searchDate = childSnapshot.val().date;
+            var searchVenue = childSnapshot.val().venue;
+            var searchCity = childSnapshot.val().city;
+            var searchState = childSnapshot.val().state;
+
+
+
+            // Add each train's data into the table
+            $("#cards").append(
+                "<div class='card' style='width: 20rem;'>" +
+                "<div class='card-block'>" +
+                "<h4 class='card-title'>" + searchName + "</h4>" + "<hr>" +
+                "<p class='card-text'>" + searchDate + "</p>" + "</br>" +
+                "<p class='card-text'>" + searchVenue + "</p>" + "</br>" +
+                "<p class='card-text'>" + searchCity + "</p>" + "</br>" +
+                "<p class='card-text'>" + searchState + "</p>" + "</br>" +
+                "<a href='#' class='btn btn-primary'> Save </a>" +
+                "</div>" +
+                "</div>"
+            );
+
+        });
     });
-
 
 });
